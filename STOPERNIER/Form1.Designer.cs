bf14fb2f680a02b7ed21b02824a9a59f1ab4a295
Piotr Chosiński <Mefriss>
2020-08-27 01:47:29 +0200

    StoopidTimer

---
+++ STOPERNIER/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace STOPERNIER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dpadUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dpadUpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dpadUpToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dpadUpToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(213, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(102, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 73);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "00";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(353, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(200, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 73);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "00";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(-1, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 73);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripLabel1,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(304, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Cycle";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Toggle gamepad control";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(95, 22);
+            this.toolStripLabel1.Text = "Pad control: OFF";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.selectToolStripMenuItem,
+            this.dpadUpToolStripMenuItem,
+            this.dpadUpToolStripMenuItem1,
+            this.dpadUpToolStripMenuItem2,
+            this.dpadUpToolStripMenuItem3});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
+            this.toolStripDropDownButton1.Text = "Set hotkey";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // selectToolStripMenuItem
+            // 
+            this.selectToolStripMenuItem.Name = "selectToolStripMenuItem";
+            this.selectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.selectToolStripMenuItem.Text = "Select";
+            this.selectToolStripMenuItem.Click += new System.EventHandler(this.selectToolStripMenuItem_Click);
+            // 
+            // dpadUpToolStripMenuItem
+            // 
+            this.dpadUpToolStripMenuItem.Name = "dpadUpToolStripMenuItem";
+            this.dpadUpToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dpadUpToolStripMenuItem.Text = "DpadUp";
+            this.dpadUpToolStripMenuItem.Click += new System.EventHandler(this.dpadUpToolStripMenuItem_Click);
+            // 
+            // dpadUpToolStripMenuItem1
+            // 
+            this.dpadUpToolStripMenuItem1.Name = "dpadUpToolStripMenuItem1";
+            this.dpadUpToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.dpadUpToolStripMenuItem1.Text = "DpadDown";
+            this.dpadUpToolStripMenuItem1.Click += new System.EventHandler(this.dpadUpToolStripMenuItem1_Click);
+            // 
+            // dpadUpToolStripMenuItem2
+            // 
+            this.dpadUpToolStripMenuItem2.Name = "dpadUpToolStripMenuItem2";
+            this.dpadUpToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.dpadUpToolStripMenuItem2.Text = "DpadLeft";
+            this.dpadUpToolStripMenuItem2.Click += new System.EventHandler(this.dpadUpToolStripMenuItem2_Click);
+            // 
+            // dpadUpToolStripMenuItem3
+            // 
+            this.dpadUpToolStripMenuItem3.Name = "dpadUpToolStripMenuItem3";
+            this.dpadUpToolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.dpadUpToolStripMenuItem3.Text = "DpadRight";
+            this.dpadUpToolStripMenuItem3.Click += new System.EventHandler(this.dpadUpToolStripMenuItem3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Min";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(135, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Sec";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(232, 110);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "miliSec";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 201);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(320, 240);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Jeakereks SpeedrunM8";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dpadUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dpadUpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dpadUpToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem dpadUpToolStripMenuItem3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ STOPERNIER/Form1.cs
@@ -0,0 +1,285 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace STOPERNIER
+{
+
+    public partial class Form1 : Form
+    {
+        Controller controller;
+        int Reset_button_selected;
+        public bool connected, pad_control = false;
+        int min, sec, msec;
+        private int buttonreset = 0;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)//start
+        {
+            timer1.Enabled = true;
+        }
+        public void xone_controller()
+        {
+            controller = new Controller(UserIndex.One);
+        }
+
+        private void button3_Click(object sender, EventArgs e)//reset
+        {
+            min = 0;
+            sec = 0;
+            msec = 0;
+            Time_disp();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            msecup();
+            Time_disp();
+        }
+
+        private void label1_Click(object sender, EventArgs e)//minutues
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //stop
+        {
+            timer1.Enabled = false;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)//seconds
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)//mseconds
+        {
+
+        }
+        private void Time_disp()
+        {
+            label1.Text = min.ToString("00");
+            label2.Text = sec.ToString("00");
+            label4.Text = msec.ToString("00");
+        }
+        private void minup()
+        {
+            min++;
+        }
+        private void secup()
+        {
+            if (sec == 59)
+            {
+                msec = 0;
+                minup();
+            }
+            else
+                sec++;
+        }
+        private void msecup()
+        {
+            if (msec == 99)
+            {
+                msec = 0;
+                secup();
+            }
+            else
+                msec++;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Button_reset();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            controller.GetState(out var state);
+            switch (Reset_button_selected)
+            {
+                case 0:
+                    if (state.Gamepad.Buttons == GamepadButtonFlags.Start)
+                    {
+                        Thread.Sleep(100);
+                        Button_reset(10);
+                        Thread.Sleep(100);
+                    }
+                    break;
+                case 1:
+                    if (state.Gamepad.Buttons == GamepadButtonFlags.Back)
+                    {
+                        Thread.Sleep(100);
+                        Button_reset(10);
+                        Thread.Sleep(100);
+                    }
+                    break;
+                case 2:
+                    if (state.Gamepad.Buttons == GamepadButtonFlags.DPadUp)
+                    {
+                        Thread.Sleep(100);
+                        Button_reset(10);
+                        Thread.Sleep(100);
+                    }
+                    break;
+                case 3:
+                    if (state.Gamepad.Buttons == GamepadButtonFlags.DPadDown)
+                    {
+                        Thread.Sleep(100);
+                        Button_reset(10);
+                        Thread.Sleep(100);
+                    }
+                    break;
+                case 4:
+                    if (state.Gamepad.Buttons == GamepadButtonFlags.DPadLeft)
+                    {
+                        Thread.Sleep(100);
+                        Button_reset(10);
+                        Thread.Sleep(100);
+                    }
+                    break;
+                case 5:
+                    if (state.Gamepad.Buttons == GamepadButtonFlags.DPadRight)
+                    {
+                        Thread.Sleep(100);
+                        Button_reset(10);
+                        Thread.Sleep(100);
+                    }
+                    break;
+            }
+            
+
+
+
+        }
+
+        private void dpadUpToolStripMenuItem1_Click(object sender, EventArgs e)//dpaddown
+        {
+            Reset_button_selected = 3;
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)//start
+        {
+            Reset_button_selected = 0;
+        }
+
+        private void selectToolStripMenuItem_Click(object sender, EventArgs e)//select
+        {
+            Reset_button_selected = 1;
+        }
+
+        private void dpadUpToolStripMenuItem_Click(object sender, EventArgs e)//dpadup
+        {
+            Reset_button_selected = 2;
+        }
+
+        private void dpadUpToolStripMenuItem2_Click(object sender, EventArgs e)//dpadleft
+        {
+            Reset_button_selected = 4;
+        }
+
+        private void dpadUpToolStripMenuItem3_Click(object sender, EventArgs e)//dpadright
+        {
+            Reset_button_selected = 5;
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)//init pad key
+        {
+            pad_control = !pad_control;
+            if (pad_control)
+            {
+                xone_controller();
+                toolStripLabel1.Text = "Pad Control: ON";
+                timer2.Enabled = true;
+                
+            }
+            else
+            {
+                toolStripLabel1.Text = "Pad Control: OFF";
+                timer2.Enabled = false;
+            }
+                
+        }
+
+        private void Button_reset(int debouncing_offset = 0)
+        {
+            if (buttonreset == 0)
+            {
+                timer1.Enabled = true;
+                buttonreset++;
+            }
+            else if (buttonreset == 1)
+            {
+                timer1.Enabled = false;
+                buttonreset++;
+            }
+            else if(buttonreset == 2){
+                buttonreset = 1;
+                startover(debouncing_offset);
+            }
+        }
+        private void startover(int debouncing_offset = 0)
+        {
+            min = 0;
+            sec = 0;
+            msec = debouncing_offset;
+            timer1.Enabled = true;
+            
+        }
+    }
+}
